39e54e8db86a231097c4627700c5649ae5620efd
ahindra <ahindrabasu@gmail.com>
2019-11-26 01:51:19 +0530

    just push the changes for branch

--- basic.py
+++ basic.py
@@ -6,7 +6,7 @@ image = cv2.imread("banner.jpg")
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Over the Clouds", image)
 cv2.imshow("Over the Clouds - gray", gray_image)
-cv2.waitKey(3000)
+cv2.waitKey(5000)
 cv2.destroyAllWindows()
 print(image)
 
